1551547c5b82a94304613ec738a3ed6ce5736f04
Rachel Kendra <kendr1rl@cmich.edu>
2013-03-21 08:38:59 -0400

    hand  3/21/13

--- hand.java
+++ hand.java
@@ -1,3 +1,37 @@
-public class hand
+public class Hand
 {
-}
+	private Card cards[];
+	private int handValue;
+	private int numCards;
+	
+	public Hand()
+	{
+		numCards = 0;
+		handValue = 0;
+		cards = new int[11];
+	}
+
+	public int getHandValue()
+	{
+		return handValue;
+	}
+	
+	public void addCard(Card newCard)
+	{
+		int index = 0;
+		while (cards[i] != null)
+			index++;
+		cards[i] = newCard;
+	}
+	
+	public void displayHand()
+	{
+		System.out.print("Cards in hand: ");
+		int index = 0;
+		while(cards[i] != null)
+		{
+			//print suit
+			//print faceValue
+		}
+	}
+}//end hand
