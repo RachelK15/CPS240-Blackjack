cd1b4dc52f22c3bb43d1b2ae1b4dc16dca97bb1c
Rachel Kendra <kendr1rl@cmich.edu>
2013-03-21 20:28:06 -0400

    Hand.java 3/21/13

--- hand.java
+++ hand.java
@@ -1,6 +1,6 @@
 public class Hand
 {
-	private Card cards[];
+	private Card[] cards;
 	private int handValue;
 	private int numCards;
 	
@@ -8,7 +8,9 @@ public class Hand
 	{
 		numCards = 0;
 		handValue = 0;
-		cards = new int[11];
+		cards = new Card[11];
+		for(int i = 0; i < 11; i++)
+			cards[i] = null;
 	}
 
 	public int getHandValue()
@@ -19,19 +21,65 @@ public class Hand
 	public void addCard(Card newCard)
 	{
 		int index = 0;
-		while (cards[i] != null)
+		while (cards[index] != null)
 			index++;
-		cards[i] = newCard;
+		cards[index] = newCard;
+		numCards++;
+		setHandValue();
 	}
 	
 	public void displayHand()
 	{
 		System.out.print("Cards in hand: ");
 		int index = 0;
-		while(cards[i] != null)
+		while(cards[index] != null)
 		{
-			//print suit
-			//print faceValue
+			cards[index].displayFaceValue();
+			System.out.print(" ");
+			index++;
+		}
+		System.out.println();
+	}
+	
+	private boolean checkBust()
+	{
+		if(handValue > 21)
+			return true;
+		else
+			return false;
+	}
+	
+	public int getNumCards()
+	{
+		return numCards;
+	}
+	
+	private boolean check21()
+	{
+		if(handValue == 21)
+			return true;
+		else
+			return false;
+	}
+	
+	private void setHandValue()
+	{
+		int index = 0;
+		int numAces = 0;
+		handValue = 0;
+		while(cards[index] != null)
+		{
+			if(cards[index].getGameValue() == 1)
+			{
+				numAces++;
+			}
+			handValue += cards[index].getGameValue();
+			index++;
+		}
+		for(int i = 0; i < numAces; i++)
+		{
+			if(handValue + 10 <= 21)
+				handValue += 10;
 		}
 	}
 }//end hand
