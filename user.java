da6b0210b81d0d77fe7620b18f71001b17890b43
Rachel Kendra <kendr1rl@cmich.edu>
2013-03-25 09:21:07 -0400

    User.java 3/25/13

--- user.java
+++ user.java
@@ -1,3 +1,17 @@
-public class user
+public class User
 {
-}
+	public User()
+	{
+	}
+	
+	public Card hit(Deck deck)
+	{
+		System.out.println("Hit");
+		return deck.getCard();
+	}
+
+	public void stand()
+	{
+		System.out.println("Stand");	
+	}
+}//end User
